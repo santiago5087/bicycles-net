fc18484242c6f6ca8aefd3464f4079471634000e
Santiago Marín <esmarina@unal.edu.co>
2020-04-11 10:26:36 -0500

    test heroku 1

--- app.js
+++ app.js
@@ -79,11 +79,17 @@ app.use('/api/bicycles', validateUser, bicyclesAPIRouter);
 app.use('/api/users', usersAPIRouter);
 
 //Permisos de la API
-app.get('/auth/google', 
-  passport.authenticate('google', {scope: [
-    'https://www.googleapis.com/auth/plus.login',
-    'https://www.googleapis.com/auth/plus.profile.emails.read']}
-    ));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/plus.login",
+      "https://www.googleapis.com/auth/plus.profile.emails.read",
+      "profile",
+      "email"
+    ]
+  })
+);
 
 app.get('/auth/google/callback', passport.authenticate('google', {
   successRedirect: '/',
